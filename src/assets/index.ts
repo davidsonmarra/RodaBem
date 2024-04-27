28b7eb85ba82d060fc77237be14e23999ee648ef
Davidson Marra <davidson.vieira@stone.com.br>
2024-04-26 21:57:09 -0300

    feat: add car svg

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,3 +1,4 @@
 export {default as AddIcon} from './icons/add';
 export {default as ErrorSvg} from './icons/error';
+export {default as CarSvg} from './icons/car';
 export {default as BackSvg} from './icons/back';
